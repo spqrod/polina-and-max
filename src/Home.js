4dd33ed013916734a99e0a6ae236e2d169238880
rodionion <rodionvh@gmail.com>
2025-02-18 10:36:04 -0300

    Add basic react structure

---
+++ src/Home.js
@@ -0,0 +1,183 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="App">
+
+      <section className='hello section'>
+        <h1>Hello and Welcome to our Wedding!</h1>
+        <p>
+          We’ve chosen to have our wedding in Ammarnäs because it’s a place we love and visit every summer to recharge. It’s one of our favorite spots in the world, and we’re excited to share it with you.
+
+          Though it might be a bit remote, its beauty makes every journey worthwhile. There’s plenty to do, and we hope you’ll enjoy exploring this scenic corner of the world with us.
+
+          The ceremony will take place near the spot where Max proposed, in Jillesnåle (Gillesnuole).
+
+          We hope you can join us for our wedding!
+
+          This is an active website, and we’ll be updating it regularly, so be sure to check back. If you have any questions, feel free to reach out to us.
+        </p>
+      </section>
+      <section className='howToGetThere section'>
+        <h2>
+          How to get here 
+        </h2>
+        <h3>
+          Plane
+        </h3>
+        <p>
+          Arvidsjaurs, Lycksele, Hemavans and Umeå are the nearby airports. 
+
+          Arvidsjaurs is the closest airport, 183 km from Ammarnäs.
+
+          If you fly to Arvidsjaur (AJR) with popular.com from Stockholm (ARN) on July 31st, 2025, we will arrange a pre-booked bus from the airport to Ammarnäs for our guests. The same applies for the return flight on August 3rd. Please let us know if you plan to take these flights.
+        </p>
+        <h3>
+          Car
+        </h3>
+        <p>
+          By car, drive to Sorsele via road 363 or E45. From Sorsele, continue on road 363 to Ammarnäs. While there is a place to refuel in Ammarnäs, it’s an additional 89 km, so you may want to refuel in Sorsele before driving the final stretch.
+
+          If you have space in your car or need a ride, please post in the WhatsApp group.
+        </p>
+        <h3>
+          Train 
+        </h3>
+        <p>
+          You can take the train up to the north, and from there, buses connect to Sorsele. From Sorsele, there is a bus service, running once or twice a day, that will take you to Ammarnäs.
+
+          SJ and tabussen.nu provide more detailed information based on your departure station.
+
+          The best options are to take an overnight SJ train to either Östersund, Umeå, or Jörn, and then take a bus or rent a car from there. (It’s better to rent a car in Umeå as it’s closer to Ammarnäs; there is no car rental in Jörn.)
+
+          The bus from Östersund to Sorsele takes about 6 hours, and from Sorsele to Ammarnäs, it’s an additional 1.5 hours.
+
+          Alternatively, there’s the tourist train (Inlandsbanan) from Östersund to Sorsele, which takes about 7 hours (a charming, slightly slow train with beautiful views).
+
+          There is also a bus from Umeå to Lycksele, and then from Lycksele to Sorsele.
+        </p>
+        <h3>
+          Hiking to Ammarnäs
+        </h3>
+        <p>
+          You can also hike to Ammarnäs. The King's Trail passes through Ammarnäs and from Hemavan it is 79 km. It is possible to hike between STF's staffed cabins when you come from Hemavan, which then takes 5-7 days. From Kvikkjokk it is 157 km. For more information follow this link https://www.swedishtouristassociation.com/trails/signature-trail-kungsleden-hemavan/ 
+        </p>
+        <h3>
+          Helicopter 
+        </h3>
+        <p>
+          There are also regular helicopter tours from Hemavan to Ammarnäs if you don’t want to hike the whole way https://arcticairhemavantarnaby.se/ 
+        </p>
+      </section>
+
+      <section className='accomodation section'>
+        <h2>Accomodation</h2>
+        <p>
+          The party will be held at Ammarnäsgården Fjällhotell & Vandrarhem, where we have reserved rooms and are in the process of arranging discounts for guests. More information will be available soon.
+
+          If you’d prefer to stay elsewhere, please refer to this page for more options.
+          https://www.visitammarnas.se/boende 
+        </p>
+      </section>
+
+      <section className='schedule section'>
+        <h2>
+          Schedule
+        </h2>
+
+        <p>
+          Thursday - Arrival Day and BBQ on Potato Hill
+          If the weather isn't ideal, we’ll move the food and karaoke to the hotel instead.
+
+          Friday - Wedding Day
+          Breakfast at the hotel, followed by getting dressed and ready.
+          The wedding bus will pick everyone up from Ammarnäs around 12:30 and take us to the ceremony location at Jillesnåle (Gillesnuole), where the ceremony will begin around 13:30. Afterward, there will be drinks and a light lunch.
+          At 16:00, the wedding bus will return everyone to Ammarnäs.
+          At 18:00, dinner and the party will begin at Ammarnäsgården Fjällhotell & Vandrarhem.
+          During dinner, there will be a kids' corner with toys (but no supervision).
+
+          Saturday - Activities Day (weather permitting)
+          Ammarnäs may feel remote, but it’s a hub for many nature-related activities. We encourage you to take this day to relax and experience the true beauty of northern nature. Please refer to the Activities Page for more details on what you can do here.
+
+          In the evening, we’ll have a party at the hotel bar.
+
+          For those interested, we’d be happy if you joined us for a hike to the top of Gáisátje (Gajssietjåhkka) around lunchtime. The hike is 4 km from the Tjulträsk parking lot to the summit. The first 2 km follow a stream through mountain birch forest. Once at the top (1008 m), you’ll be rewarded with a view of the entire reserve. The hike is a relatively easy walk through mountain moorland, with excellent birdwatching opportunities. Difficulty: Medium – the hike includes a 456-meter elevation gain.
+
+          For those who prefer a round-trip hike from Ammarnäs, you can follow the Ammarnäs-Tjulträsk parking lot trail (8.2 km), then continue from Tjulträsk parking lot to Gáisátje (4 km). From Gáisátje, you can hike via Karsbäcken to Småfjällen and then join the King’s Trail back to Ammarnäs, which is about 10 km (see the highlighted trail in yellow below).
+
+          For more info please see the map below  https://www.naturkartan.se/sv/vasterbottens-lan/gajsatjakke?utm_source=naturkartan&utm_medium=map 
+
+          Sunday - Departure Day
+          For those taking the Popular flight to Stockholm (ARN) at 13:00, we will arrange a bus to drive from Ammarnäs to Arvidsjaur Airport in the morning.
+
+        </p>
+      </section>
+
+      <section className='activities section'>
+        <h3>
+          Activities
+        </h3>
+        <p>
+          Fishing 
+          Horse Riding 
+          Hiking 
+          Birdwatching 
+          Sámi Handicraft
+          Naturum
+          Potato Hill
+          Swimming 
+          Brudslöjan
+          Gimegolts
+          Geunja Sami Ecolodge
+          Cold-Nose Huskies 
+        </p>
+      </section>
+
+      <section className='RSVP section'>
+        <h2>
+          RSVP
+        </h2>
+        <p>
+          Please register kids separately and indicate if they require a children's chair.
+          Name and Surname 
+          Dietary requirements 
+          Drinking requirements (Alcohol or no alcohol)
+          Other 
+        </p>
+      </section>
+
+      <section className='weddingDetails section'>
+        <h3>
+          Wedding Details
+        </h3>
+        <h3>
+          Dress code
+        </h3>
+        <p>
+          We are in the wild, so wear your wildest outfit. 
+        </p>
+        <h3>
+          Toast masters
+        </h3>
+        <p>
+          In a traditional Crimean wedding, proposing toasts is as natural as taking a breath, so we’d be happy to hear if you’d like to say something. At the end of the toast, you might want to say “Gorka,” which means "bitter," which obviously signals that Max and I have to kiss. For an even more traditional touch, you can start counting to 10, and Max and I will only stop kissing once you reach 10.
+        </p>
+        <h3>
+          What to pack 
+        </h3>
+        <p>
+          The weather in the mountains can change quickly, so please check the forecast before you travel. Be sure to bring mosquito repellent with you.
+        </p>
+        <h3>
+          Included with our celebration
+        </h3>
+        <p>
+          BBQ and welcome drink on Thursday, along with all food and drinks on the wedding day.
+        </p>
+      </section>
+
+    </div>
+  );
+}
+
+export default Home;
